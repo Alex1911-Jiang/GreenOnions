ca615b8cf71107d632ba7f5fbb1fcb8cc2c18bd2
Executor <exprotected@outlook.com>
2022-01-13 22:24:34 +0800

    迁移剩余的 events 至现在的 Handler see Executor-Cheng/Mirai-CSharp#111

--- GreenOnions.BotMain/GroupMessage.cs
+++ GreenOnions.BotMain/GroupMessage.cs
@@ -1,6 +1,7 @@
 ﻿using GreenOnions.PictureSearcher;
 using GreenOnions.Repeater;
 using GreenOnions.Utility;
+using Mirai.CSharp.Builders;
 using Mirai.CSharp.HttpApi.Handlers;
 using Mirai.CSharp.HttpApi.Models.ChatMessages;
 using Mirai.CSharp.HttpApi.Models.EventArgs;
@@ -8,29 +9,26 @@ using Mirai.CSharp.HttpApi.Parsers;
 using Mirai.CSharp.HttpApi.Parsers.Attributes;
 using Mirai.CSharp.HttpApi.Session;
 using Mirai.CSharp.Models;
-using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace GreenOnions.BotMain
 {
     [RegisterMiraiHttpParser(typeof(DefaultMappableMiraiHttpMessageParser<IGroupMessageEventArgs, GroupMessageEventArgs>))]
-    public class GroupMessage : IMiraiHttpMessageHandler<IGroupMessageEventArgs>
+    [RegisterMiraiHttpParser(typeof(DefaultMappableMiraiHttpMessageParser<IGroupMemberJoinedEventArgs, GroupMemberJoinedEventArgs>))]
+    [RegisterMiraiHttpParser(typeof(DefaultMappableMiraiHttpMessageParser<IGroupMemberPositiveLeaveEventArgs, GroupMemberPositiveLeaveEventArgs>))]
+    [RegisterMiraiHttpParser(typeof(DefaultMappableMiraiHttpMessageParser<IGroupMemberKickedEventArgs, GroupMemberKickedEventArgs>))]
+    public class GroupMessage : IMiraiHttpMessageHandler<IGroupMessageEventArgs>,
+                                IMiraiHttpMessageHandler<IGroupMemberJoinedEventArgs>,
+                                IMiraiHttpMessageHandler<IGroupMemberPositiveLeaveEventArgs>,
+                                IMiraiHttpMessageHandler<IGroupMemberKickedEventArgs>
     {
         public async Task HandleMessageAsync(IMiraiHttpSession session, IGroupMessageEventArgs e)
         {
-            e.BlockRemainingHandlers = false;
-
-            if (BotInfo.BannedGroup.Contains(e.Sender.Group.Id)) return;
-            if (BotInfo.BannedUser.Contains(e.Sender.Id)) return;
-            if (BotInfo.DebugMode)
+            if (!CheckPreconditions(e.Sender))
             {
-                if (BotInfo.DebugReplyAdminOnly)
-                    if (!BotInfo.AdminQQ.Contains(e.Sender.Id))
-                        return;
-                if (BotInfo.OnlyReplyDebugGroup)
-                    if (!BotInfo.DebugGroups.Contains(e.Sender.Group.Id))
-                        return;
+                return;
             }
 
             QuoteMessage quoteMessage = new QuoteMessage((e.Chain[0] as SourceMessage).Id, e.Sender.Group.Id, e.Sender.Id, e.Sender.Id);
@@ -103,6 +101,101 @@ namespace GreenOnions.BotMain
                 }
                 #endregion -- 复读 --
             }
+        }
+
+        public Task HandleMessageAsync(IMiraiHttpSession session, IGroupMemberJoinedEventArgs e)
+        {
+            if (!CheckPreconditions(e.Member) || !BotInfo.SendMemberJoinedMessage)
+            {
+                return Task.CompletedTask;
+            }
+            return session.SendGroupMessageAsync(e.Member.Group.Id, ReplaceMessage(session.GetMessageChainBuilder(), BotInfo.MemberJoinedMessage, e.Member));
+        }
+
+        public Task HandleMessageAsync(IMiraiHttpSession session, IGroupMemberPositiveLeaveEventArgs e)
+        {
+            if (!CheckPreconditions(e.Member) || !BotInfo.SendMemberPositiveLeaveMessage)
+            {
+                return Task.CompletedTask;
+            }
+            return session.SendGroupMessageAsync(e.Member.Group.Id, ReplaceMessage(session.GetMessageChainBuilder(), BotInfo.MemberPositiveLeaveMessage, e.Member));
+        }
+
+        public Task HandleMessageAsync(IMiraiHttpSession session, IGroupMemberKickedEventArgs e)
+        {
+            if (!CheckPreconditions(e.Member) || !BotInfo.SendMemberBeKickedMessage)
+            {
+                return Task.CompletedTask;
+            }
+            return session.SendGroupMessageAsync(e.Member.Group.Id, ReplaceMessage(session.GetMessageChainBuilder(), BotInfo.MemberBeKickedMessage, e.Member, e.Operator));
+        }
+
+        private static readonly Regex regexTags = new Regex("<@?成员QQ>|<成员昵称>|<@?操作者QQ>|<操作者昵称>");
+
+        private static bool CheckPreconditions(IGroupMemberInfo e)
+        {
+            if (BotInfo.BannedGroup.Contains(e.Group.Id) ||
+                BotInfo.BannedUser.Contains(e.Id))
+            {
+                return false;
+            }
+            if (BotInfo.DebugMode)
+            {
+                if (BotInfo.DebugReplyAdminOnly)
+                    if (!BotInfo.AdminQQ.Contains(e.Id))
+                        return false;
+                if (BotInfo.OnlyReplyDebugGroup)
+                    if (!BotInfo.DebugGroups.Contains(e.Group.Id))
+                        return false;
+            }
+            return true;
+        }
+
+        private static IMessageChainBuilder ReplaceMessage(IMessageChainBuilder builder, string messageCmd, IGroupMemberInfo member, IGroupMemberInfo Operator = null)
+        {
+            string remainMessage = messageCmd;
+            foreach (Match match in regexTags.Matches(messageCmd))
+            {
+                string matchString = match.ToString();
+                int index = remainMessage.IndexOf(matchString);
+                string left = remainMessage.Substring(0, index);
+                if (!string.IsNullOrEmpty(left))
+                {
+                    builder.AddPlainMessage(left);
+                }
+                string right = remainMessage.Substring(index + matchString.Length);
+                remainMessage = right;
+
+                if (match.ToString() == "<@成员QQ>")
+                {
+                    builder.AddAtMessage(member.Id);
+                }
+                else if (match.ToString() == "<成员QQ>")
+                {
+                    builder.AddPlainMessage(member.Id.ToString());
+                }
+                else if (match.ToString() == "<成员昵称>")
+                {
+                    builder.AddPlainMessage(member.Name);
+                }
+                else if (Operator != null)
+                {
+                    if (match.ToString() == "<@操作者QQ>")
+                    {
+                        builder.AddAtMessage(Operator.Id);
+                    }
+                    else if (match.ToString() == "<操作者QQ>")
+                    {
+                        builder.AddPlainMessage(Operator.Id.ToString());
+                    }
+                    else if (match.ToString() == "<操作者昵称>")
+                    {
+                        builder.AddPlainMessage(Operator.Name);
+                    }
+                }
+            }
+            builder.AddPlainMessage(remainMessage);
+            return builder;
         }
     }
 }
