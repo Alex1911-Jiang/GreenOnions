ae745b6dc0ac77a906acb090e781e2768de70aac
Alex1911-Jiang <tjxx_jgd@163.com>
2025-02-02 23:11:04 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get update &&
-sudo apt-get install -y dotnet-sdk-8.0
-sudo apt install wget
-sudo apt install unzip
-sudo apt install screen
-mkdir GreenOnions
-cd GreenOnions
-sudo wegt https://github.com/Alex1911-Jiang/GreenOnions/releases/download/控制台/GreenOnions.NT.zip
-dotnet GreenOnions.NT.dll
